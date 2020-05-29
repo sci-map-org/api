4159d841ca64c8187e7ab533760f0363b93c275c
Olivier Ramier <olivier.ramier@gmail.com>
2020-05-29 16:05:34 +0200

    neo4j version + setup docker / AWS beanstalk

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,4 +1,5 @@
 # If the directory, `dist`, doesn't exist, create `dist`
 stat dist || mkdir dist
 # Archive artifacts
-zip $npm_package_name.zip -r src dist package.json yarn.lock .elasticbeanstalk
+# zip $npm_package_name.zip -r src dist package.json yarn.lock .elasticbeanstalk
+zip $npm_package_name.zip Dockerrun.aws.json
--- src/infra/neo4j.ts
+++ src/infra/neo4j.ts
@@ -1,5 +1,9 @@
 import neo4j from 'neo4j-driver';
-
 import { env } from '../env';
 
 export const neo4jDriver = neo4j.driver(env.NEO4J.DATABASE_URL, neo4j.auth.basic(env.NEO4J.USER, env.NEO4J.PASSWORD));
+
+process.on('exit', () => {
+  neo4jDriver.close();
+  console.log('Closing driver');
+});
