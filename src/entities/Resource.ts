353ed9e844ab4c1468ab9172d93d5f4b81d9e47b
Olivier Ramier <olivier.ramier@gmail.com>
2020-09-03 02:53:09 +0200

    update types resource

--- src/entities/Resource.ts
+++ src/entities/Resource.ts
@@ -1,33 +1,29 @@
-// polymorphic ?
 export const ResourceLabel = 'Resource';
+
 export enum ResourceMediaType {
   video = 'video',
   text = 'text',
   audio = 'audio',
+  image = 'image',
+  interactive_content = 'interactive_content',
 }
 
 export enum ResourceType {
   article = 'article',
   article_series = 'article_series',
-  tutorial = 'tutorial',
-  introduction = 'introduction',
   course = 'course',
-  guide = 'guide',
+  youtube_video = 'youtube_video',
+  book = 'book',
   podcast = 'podcast',
   podcast_series = 'podcast_series',
+  talk = 'talk',
+  documentary = 'documentary',
+  website = 'website',
+  video_game = 'video_game',
+  infographic = 'infographic',
+  tweet = 'tweet',
   other = 'other',
 }
-
-// export enum PedagogicalApproach {
-//   practical = 'practical',
-//   theoretical = 'theoretical',
-//   intuitive = 'intuitive',
-//   gamified = 'gamified',
-//   visual = 'visual',
-//   interactive = 'interactive',
-//   abstract = 'abstract',
-//   detailed = 'detailed',
-// }
 
 export interface Resource {
   _id: string;
