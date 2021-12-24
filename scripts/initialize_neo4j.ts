31f74485c49d63339f19c0e38860fc7abdee41f9
Olivier Ramier <olivier.ramier@gmail.com>
2021-12-24 17:45:36 +0100

    add migrations topic types

--- scripts/initialize_neo4j.ts
+++ scripts/initialize_neo4j.ts
@@ -8,3 +8,16 @@
 // CREATE CONSTRAINT unique_topic_ids ON (n:Topic) ASSERT n._id IS UNIQUE
 // CREATE CONSTRAINT unique_learning_material_ids ON (n:LearningMaterial) ASSERT n._id IS UNIQUE
 // CREATE CONSTRAINT unique_learning_goals_ids ON (n:LearningGoal) ASSERT n._id IS UNIQUE
+// CREATE CONSTRAINT unique_topic_types_names ON (n:TopicType) ASSERT n.name IS UNIQUE
+// seed topic types
+// WITH [{ name: 'concept', color: 'orange' },
+// { name: 'field', color: 'red' },
+// { name: 'tool', color: 'green' },
+// { name: 'problem', color: 'green' },
+// { name: 'theory', color: 'red' },
+// { name: 'method', color: 'orange' },
+// { name: 'application', color: 'green' }] AS seeds
+// UNWIND seeds AS seed
+// MERGE (n:TopicType {name: seed.name})
+// SET n.color = seed.color
+// RETURN n
